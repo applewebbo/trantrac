6c6692f1142e5033ae65493c2e8073eec0fcbb3e
WEBBOgrafico <e.bonardi@me.com>
2025-01-23 09:21:56 +0100

    configured google stuff for saving to sheet (for #1 and for #3)

--- core/settings.py
+++ core/settings.py
@@ -180,3 +180,19 @@ ACCOUNT_LOGOUT_REDIRECT_URL = "index"
 # CRISPY TAILWIND
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
+
+# GOOGLE SHEETS API
+GOOGLE_SHEETS_CREDENTIALS = {
+    "type": "service_account",
+    "project_id": env("GOOGLE_SHEETS_PROJECT_ID"),
+    "private_key_id": env("GOOGLE_SHEETS_PRIVATE_KEY_ID"),
+    "private_key": env("GOOGLE_SHEETS_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": env("GOOGLE_SHEETS_CLIENT_EMAIL"),
+    "client_id": env("GOOGLE_SHEETS_CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": env("GOOGLE_SHEETS_CLIENT_X509_CERT_URL"),
+}
+
+GOOGLE_SHEETS_SPREADSHEET_ID = env("GOOGLE_SHEETS_SPREADSHEET_ID")
--- trantrac/views.py
+++ trantrac/views.py
@@ -1,7 +1,38 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 from trantrac.forms import TransactionForm
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+
+def save_to_sheet(values):
+    credentials = service_account.Credentials.from_service_account_info(
+        settings.GOOGLE_SHEETS_CREDENTIALS, scopes=SCOPES
+    )
+    service = build("sheets", "v4", credentials=credentials)
+
+    try:
+        body = {"values": values}
+        result = (
+            service.spreadsheets()
+            .values()
+            .append(
+                spreadsheetId=settings.GOOGLE_SHEETS_SPREADSHEET_ID,
+                range="Foglio1!A:C",
+                valueInputOption="USER_ENTERED",
+                insertDataOption="INSERT_ROWS",
+                body=body,
+            )
+            .execute()
+        )
+
+        return result.get("updates").get("updatedRows") == 1
+    finally:
+        service.close()
 
 
 @login_required
@@ -9,9 +40,19 @@ def index(request):
     if request.method == "POST":
         form = TransactionForm(request.POST)
         if form.is_valid():
-            amount = form.cleaned_data["amount"]
-            print(amount)
-            return render(request, "trantrac/transaction_ok.html")
+            values = [
+                [
+                    str(request.user),
+                    form.cleaned_data["date"].strftime("%Y-%m-%d"),
+                    str(form.cleaned_data["amount"]),
+                ]
+            ]
+
+            if save_to_sheet(values):
+                return render(request, "trantrac/transaction_ok.html")
+            else:
+                return render(request, "trantrac/transaction_error.html")
+
     else:
         form = TransactionForm()
 
