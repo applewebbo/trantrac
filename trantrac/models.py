ea60601a52aadb2c8d2fa7d191f3293a3ef3fbcc
WEBBOgrafico <e.bonardi@me.com>
2025-01-24 15:30:36 +0100

    added category on google sheet when saving model (fix #2)

--- trantrac/models.py
+++ trantrac/models.py
@@ -1,4 +1,35 @@
 from django.db import models
+from django.conf import settings
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+
+def save_to_sheet(values):
+    credentials = service_account.Credentials.from_service_account_info(
+        settings.GOOGLE_SHEETS_CREDENTIALS, scopes=SCOPES
+    )
+    service = build("sheets", "v4", credentials=credentials)
+
+    try:
+        body = {"values": values}
+        result = (
+            service.spreadsheets()
+            .values()
+            .append(
+                spreadsheetId=settings.GOOGLE_SHEETS_SPREADSHEET_ID,
+                range="CATEGORIE!A:A",
+                valueInputOption="USER_ENTERED",
+                insertDataOption="INSERT_ROWS",
+                body=body,
+            )
+            .execute()
+        )
+
+        return result.get("updates").get("updatedRows") == 1
+    finally:
+        service.close()
 
 
 class Category(models.Model):
@@ -10,6 +41,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        values = [[self.name]]
+        save_to_sheet(values)
+        super().save(*args, **kwargs)
 
 
 class Account(models.Model):
