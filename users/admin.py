287b3c28065bda3d6ac47f17996a3dcd6e70a8d9
WEBBOgrafico <e.bonardi@me.com>
2025-02-09 14:10:54 +0100

    fixed user creation in admin (fix #19)

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,48 @@
 from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
 from users.models import User
 
+# First unregister
 admin.site.unregister(Group)
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(UserAdmin):
+    list_display = ("email", "display_name", "is_staff")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("display_name",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "display_name", "password1", "password2"),
+            },
+        ),
+    )
 
 
+# Then register with custom admin
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, admin.ModelAdmin):
     pass
